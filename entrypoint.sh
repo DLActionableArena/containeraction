cb59e4ca707ba258d5528833047cf40047c7c8ee
Daniel Lavalliere <dlavalli@users.noreply.github.com>
2025-07-11 11:59:51 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Hello $INPUT_MYINPUT"
-memory=$(cat /proc/meminfo)
+memory=`cat /proc/meminfo`
 escaped_memory=$(printf %s "$memory")
-echo "memory=${memory}" >> "$GITHUB_OUTPUT"
+echo "memory=$memory" >> "$GITHUB_OUTPUT"
 # echo "::set-output name=key::$(expression)"
 # echo "::set-output name=memory::$memory"
